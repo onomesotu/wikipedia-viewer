cb4cf78ea1808048f559225f2b5abf2d8eed8621
onomesotu <onome.sotu@me.com>
2017-12-30 02:11:15 +0200

    Changed the margin top of the header when the search results is served to the user and added code to give focus to the input element when the on page load

--- js/main.js
+++ js/main.js
@@ -1,80 +1,87 @@
 
-const normalSearchButton = document.getElementsByClassName('buttons__search');
+document.addEventListener('DOMContentLoaded', function(e){
+	const input = document.querySelector('#search');
+	input.focus();
 
-normalSearchButton[0].addEventListener('click', makeCORSRequest, false);
-const enterKey = document.querySelector('input');
+	const normalSearchButton = document.getElementsByClassName('buttons__search');
+	normalSearchButton[0].addEventListener('click', makeCORSRequest, false);
 
-enterKey.addEventListener('keyup', function(e){
-	if(e.keyCode === 13) {
-		makeCORSRequest();
-	}
-});
+	const headerMargin = document.getElementsByClassName('header__title');
 
-function createCORSRequest(method, url) {
-	var request = new XMLHttpRequest();
-	if ('withCredentials' in request){
-		request.open(method, url, true);
-	} else if (typeof XDomainRequest != 'undefined'){
-		request = new XDomainRequest();
-		request.open(method, url);
-	} else {
-		request = null;
-	}
-	return request;
-}
-
-function makeCORSRequest(e) {
-	const inputText = document.getElementById('search').value.trim();
-	if (!inputText) {
-		return;
-	} else {
-		const wikipediaAPI = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + inputText + '&limit=10&namespace=0&origin=*&format=json';
-		const request = createCORSRequest('GET', wikipediaAPI);
-		if(!request) {
-			console.log('CORS not supported');
-			return;
+	const enterKey = document.querySelector('input');
+	enterKey.addEventListener('keyup', function(e){
+		if(e.keyCode === 13) {
+			makeCORSRequest();
 		}
+	});
 
-		request.onload = function(e){
-			if(this.readyState === 4){
-				if(this.status >= 200 && this.status <= 400){
-					return success(this.responseText);
-				} else {
-					console.error(this.statusText);
-				}
+	function createCORSRequest(method, url) {
+		var request = new XMLHttpRequest();
+		if ('withCredentials' in request){
+			request.open(method, url, true);
+		} else if (typeof XDomainRequest != 'undefined'){
+			request = new XDomainRequest();
+			request.open(method, url);
+		} else {
+			request = null;
+		}
+		return request;
+	}
+
+	function makeCORSRequest(e) {
+		const inputText = document.getElementById('search').value.trim();
+		if (!inputText) {
+			return;
+		} else {
+			const wikipediaAPI = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + inputText + '&limit=10&namespace=0&origin=*&format=json';
+			const request = createCORSRequest('GET', wikipediaAPI);
+			if(!request) {
+				console.log('CORS not supported');
+				return;
 			}
-		};
 
-		request.onerror = function(e){
-			console.log(request.statusText);
-		};
+			request.onload = function(e){
+				if(this.readyState === 4){
+					if(this.status >= 200 && this.status <= 400){
+						return success(this.responseText);
+					} else {
+						console.error(this.statusText);
+					}
+				}
+			};
 
-		request.send(null);
-	}	
-}
+			request.onerror = function(e){
+				console.log(request.statusText);
+			};
 
- function success(responseText){
- 	var response = JSON.parse(responseText);
- 	var mainDiv = document.getElementById('main');
- 	if (!mainDiv.hasChildNodes()){
- 		result(mainDiv, response);
- 	} else {
- 		mainDiv.innerHTML = "";
- 		result(mainDiv, response);
- 	}
+			request.send(null);
+		}	
+	}
 
- 	function result(div, response){
-	 	for(var i = 0; i < 10; ++i) {
-	 		let element = document.createElement('a');
-	 		element.setAttribute('href', response[3][i]);
-	 		element.setAttribute('class', 'main__anchors');
-	 		element.setAttribute('target', '_blank');
-	 		let block_div = '<div><h3>' + response[1][i] + '</h3><p class="main__paragraphs">' + response[2][i] + '</p></div></a>';
-	 		element.innerHTML = block_div;
-	 		div.appendChild(element);
+	 function success(responseText){
+	 	var response = JSON.parse(responseText);
+	 	var mainDiv = document.getElementById('main');
+	 	if (!mainDiv.hasChildNodes()){
+	 		result(mainDiv, response);
+	 		headerMargin[0].style.marginTop = '20px';
+	 	} else {
+	 		mainDiv.innerHTML = "";
+	 		result(mainDiv, response);
 	 	}
+
+	 	function result(div, response){
+		 	for(var i = 0; i < 10; ++i) {
+		 		let element = document.createElement('a');
+		 		element.setAttribute('href', response[3][i]);
+		 		element.setAttribute('class', 'main__anchors');
+		 		element.setAttribute('target', '_blank');
+		 		let block_div = '<div><h3>' + response[1][i] + '</h3><p class="main__paragraphs">' + response[2][i] + '</p></div></a>';
+		 		element.innerHTML = block_div;
+		 		div.appendChild(element);
+		 	}
+		 }
 	 }
- }
+});
 
  
 
